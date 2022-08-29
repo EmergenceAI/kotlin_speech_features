0bc9126d791e711cbd8e69034e07f1b570e83313
Raquib-ul Alam <alam.kanak@gmail.com>
2022-08-30 00:56:19 +1000

    Implement deframesig and logpowspec

--- lib/src/commonMain/kotlin/org/merlyn/kotlinspeechfeatures/SignalProc.kt
+++ lib/src/commonMain/kotlin/org/merlyn/kotlinspeechfeatures/SignalProc.kt
@@ -2,9 +2,7 @@ package org.merlyn.kotlinspeechfeatures
 
 import org.merlyn.kotlinspeechfeatures.fft.FFT
 import org.merlyn.kotlinspeechfeatures.fft.KotlinFFT
-import kotlin.math.ceil
-import kotlin.math.pow
-import kotlin.math.sqrt
+import kotlin.math.*
 
 class SignalProc(private val fft: FFT = KotlinFFT()) {
 
@@ -17,7 +15,13 @@ class SignalProc(private val fft: FFT = KotlinFFT()) {
      * @param strideTrick use stride trick to compute the rolling window and window multiplication faster
      * @return an array of frames. Size is NUMFRAMES by frame_len.
      */
-    fun framesig(signal: FloatArray, frameLen: Int, frameStep: Int, winFunc: FloatArray? = null, strideTrick: Boolean = true): Array<FloatArray> {
+    fun framesig(
+        signal: FloatArray,
+        frameLen: Int,
+        frameStep: Int,
+        winFunc: FloatArray? = null,
+        strideTrick: Boolean = true
+    ): Array<FloatArray> {
         val nFrames = if (signal.size > frameLen) {
             1 + (ceil((signal.size - frameLen) / frameStep.toFloat())).toInt()
         } else {
@@ -90,6 +94,55 @@ class SignalProc(private val fft: FFT = KotlinFFT()) {
         return response
     }
 
+
+    /**
+     * Does overlap-add procedure to undo the action of framesig.
+     * @param frames the array of frames.
+     * @param sigLen the length of the desired signal, use 0 if unknown. Output will be truncated to siglen samples.
+     * @param frameLen length of each frame measured in samples.
+     * @param frameStep number of samples after the start of the previous frame that the next frame should begin.
+     * @param winFunc the analysis window to apply to each frame. By default no window is applied.
+     * @return a 1-D signal.
+     */
+    fun deframesig(
+        frames: Array<FloatArray>,
+        sigLen: Int, frameLen: Int,
+        frameStep: Int,
+        winFunc: (Int) -> IntArray = { Array(it){ 1 }.toIntArray() }
+    ): FloatArray {
+        val numFrames = frames.size
+        if (frames[0].size != frameLen) {
+            throw IllegalArgumentException("frames matrix is wrong size, 2nd dim is not equal to frameLen")
+        }
+        val arrange= (0..frameLen).map { it.toFloat() }.toFloatArray()
+        val arrange2 = (0..numFrames).map { it * frameStep.toFloat() }.toFloatArray()
+        val indices = (MathUtils.tile(arrange, numFrames, 1) + MathUtils.transpose(MathUtils.tile(arrange2, frameLen, 1))).map {
+            it.map { it.roundToInt() }.toIntArray()
+        }
+        val padLen = (numFrames - 1) * frameStep + frameLen
+        val sigLen2 = if (sigLen <= 0) {
+            padLen
+        }
+        else {
+            sigLen
+        }
+        val windowCorrection = (0..padLen).map { 0f }.toMutableList()
+        val recSignal = (0..padLen).map { 0f }.toMutableList()
+        val win = winFunc(frameLen)
+        (0..numFrames).forEach { i ->
+            val updatedWinCorrection = indices[i].map { windowCorrection[it] } + win.map { it + 1f.pow(-15) }
+            val updatedRecSignal = indices[i].map { recSignal[it] } + frames[i].toTypedArray()
+            indices[i].forEachIndexed { index, value ->
+                windowCorrection[value] = updatedWinCorrection[index]
+                recSignal[value] = updatedRecSignal[index]
+            }
+        }
+
+        return recSignal.mapIndexed { index, value ->
+            value / windowCorrection[index]
+        }.subList(0, sigLen2).toFloatArray()
+    }
+
     /**
      * Compute the magnitude spectrum of each frame in frames. If frames is an NxD matrix, output will be Nx(NFFT/2+1).
      * @param frames the array of frames. Each row is a frame.
@@ -147,15 +200,42 @@ class SignalProc(private val fft: FFT = KotlinFFT()) {
         return preemph
     }
 
-    fun deframesig() {
-        // TODO
+    /**
+     * Compute the log power spectrum of each frame in frames. If frames is an NxD matrix, output
+     * will be Nx(NFFT/2+1).
+     * @param frames the array of frames. Each row is a frame.
+     * @param nfft the FFT length to use. If NFFT > frame_len, the frames are zero-padded.
+     * @param norm If norm=true, the log power spectrum is normalised so that the max value (across
+     * all frames) is 0.
+     * @return If frames is an NxD matrix, output will be Nx(NFFT/2+1). Each row will be the log
+     * power spectrum of the corresponding frame.
+     */
+    suspend fun logpowspec(
+        frames: Array<FloatArray>,
+        nfft: Int,
+        norm: Boolean = true
+    ): Array<FloatArray> {
+        val ps = powspec(frames, nfft)
+        val lps = ps.map { arr ->
+            return@map arr.map x@ {
+                val x = if (it <= 10f.pow(-30)) 10f.pow(-30) else it
+                return@x 10 * log10(x)
+            }.toFloatArray()
+        }.toTypedArray()
+        return if (norm) {
+            val max = lps.maxBy {
+                it.max()
+            }.max()
+            lps.map { arr ->
+                arr.map {
+                    it - max
+                }.toFloatArray()
+            }.toTypedArray()
+        } else {
+            lps
+        }
     }
-
-    fun logpowspec() {
-        // TODO
-    }
-
-
+    
     private fun modul(r: Float, i: Float): Float {
         return if (r != 0.0f || i != 0.0f) {
             sqrt(r * r + i * i)
